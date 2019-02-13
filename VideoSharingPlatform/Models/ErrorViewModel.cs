05a382dbc5cdbd33375bf787951c90f39ff17719
naps250 <naps250@gmail.com>
2019-02-13 20:14:31 +0200

    Initial commit.

---
+++ VideoSharingPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VideoSharingPlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
